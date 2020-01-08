0d8d3e1c3475473e9ae18c7c780b7aa470d2ed9b
Gautam Mishra <gautami.mishra@outlook.com>
2020-01-08 16:04:18 +1100

    Added clientName

--- sourceCode/ConstructionProject.java
+++ sourceCode/ConstructionProject.java
@@ -1,23 +1,33 @@
 public class ConstructionProject {
 
-    public String streetAddress;
-    public String suburb;
-    public String description;
-    public String photoAddress;
+  private String clientName;
+  public String getClientName() {
+    return clientName;
+  }
+
+  public String streetAddress;
+  public String suburb;
+  public String description;
+  public String photoAddress;
 //    public String[] contractorList;
 //    public String[] todoList;
 
-    public ConstructionProject(String streetAddress, String suburb, String description, String photoAddress) {
-        this.streetAddress = streetAddress;
-        this.suburb = suburb;
-        this.description = description;
-        this.photoAddress = photoAddress;
+  public ConstructionProject(String clientName, String streetAddress, String suburb, String description, String photoAddress) {
+      this.clientName = clientName;
+      this.streetAddress = streetAddress;
+      this.suburb = suburb;
+      this.description = description;
+      this.photoAddress = photoAddress;
 //        this.todoList[] = new String[todoItem];
 //        this.contractorList[] = new String[contractorItem];
     }
 
-    void listSpecifications() {
-        System.out.println("The house " + streetAddress + " is in " + suburb + ". Notes include..." + description + " and the photo address is " + photoAddress + ".");
-    }
+  void listSpecifications() {
+      System.out.println(getClientName() + "'s house " + streetAddress + " is in " + suburb + ". Notes include..." + description + " and the photo address is " + photoAddress + ".");
+  }
+
+  void listClientName() {
+    System.out.println("The client for this project is " + getClientName());
+  }
 
 }
--- sourceCode/MainProgram.java
+++ sourceCode/MainProgram.java
@@ -4,9 +4,10 @@ public class MainProgram {
 
     public static void main(String[] args) {
 
-      ConstructionProject mishraHouse = new ConstructionProject("18 Trumble Street", "Pearce", "Currently in development", "sampleHouse.jpg");
+      ConstructionProject mishraHouse = new ConstructionProject("Rajesh Mishra", "18 Trumble Street", "Pearce", "Currently in development", "sampleHouse.jpg");
 
       mishraHouse.listSpecifications();
+      mishraHouse.listClientName();
 
     }
 }
