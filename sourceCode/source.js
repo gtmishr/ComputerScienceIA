cec7c7f6b7db75caebf7c7cf5ef101cf98eba1e5
Gautam Mishra <gautami.mishra@outlook.com>
2020-01-10 13:47:55 +1100

    Something that I cannot explain happens to the buttons when you click them
    But hey progress is progress

--- sourceCode/source.js
+++ sourceCode/source.js
@@ -1,7 +1,3 @@
-function createNewButton() {
-    console.log("this is cool");
-}
-
 function searchBar() {
 
   var input = document.getElementById("searchBar");
@@ -19,6 +15,26 @@ function searchBar() {
       searchList[i].style.display = "none";
     }
 
+  }
+
+}
+
+function createNewButton() {
+  console.log("this is cool");
+}
+
+var isProjectExpanded = false;
+
+function expandProject(projectNumber) {
+
+  if (isProjectExpanded == false) {
+    console.log(isProjectExpanded);
+    document.getElementById("project" + projectNumber).style.height = "800px";
+    isProjectExpanded = true;
+  } else {
+    console.log(isProjectExpanded);
+    document.getElementById("project" + projectNumber).style.height = "250px";
+    isProjectExpanded = false;
   }
 
 }
