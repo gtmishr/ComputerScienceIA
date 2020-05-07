96bc84c283d1418380d01c773d2cd6e347ef6b2f
Gautam Mishra <gautami.mishra@outlook.com>
2020-05-07 12:33:06 +1000

    Continued development of database systems

--- NewSouthWalesBuildingCompliance/src/main/java/com/group/NewSouthWalesBuildingCompliance/datasource/Datasource.java
+++ NewSouthWalesBuildingCompliance/src/main/java/com/group/NewSouthWalesBuildingCompliance/datasource/Datasource.java
@@ -5,8 +5,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class Postgresdatasource {
+@Configuration
+public class Datasource {
 
     @Bean
     @ConfigurationProperties("app.datasource")
