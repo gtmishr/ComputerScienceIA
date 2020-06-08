0bdda371b154c7a18bc2571dc24b81f846818059
Gautam Mishra <gautami.mishra@outlook.com>
2020-06-08 11:04:49 +1000

    Duplication of Contractor class for building projects

---
+++ NewSouthWalesBuildingCompliance/src/main/java/com/group/NewSouthWalesBuildingCompliance/model/Project.java
@@ -0,0 +1,53 @@
+package com.group.NewSouthWalesBuildingCompliance.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+
+    // Private for encapsulation
+    private final String clientName;
+    private final String streetAddress;
+    private final String suburb;
+    private final String description;
+    private List<Contractor> contractorList;
+    private final String todoList;
+
+    // All of the attributes appropriate for any given building project
+
+    public Project(@JsonProperty("clientName") String clientName,
+                   @JsonProperty("streetAddress") String streetAddress,
+                   @JsonProperty("suburb") String suburb,
+                   @JsonProperty("description") String description,
+                   @JsonProperty("contractorList") List<Contractor> contractorList,
+                   @JsonProperty("todoList") String todoList) {
+
+        this.clientName = clientName;
+        this.streetAddress = streetAddress;
+        this.suburb = suburb;
+        this.description = description;
+        this.contractorList = contractorList;
+        this.todoList = todoList;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+    public String getSuburb() {
+        return suburb;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public List<Contractor> getContractorList() {
+        return contractorList;
+    }
+    public String getTodoList() {
+        return todoList;
+    }
+}
